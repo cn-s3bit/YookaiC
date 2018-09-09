b9e005b4658149ad891a66df9695a1743c968d98
北海若 <shiruoxi2010@163.com>
2018-09-09 09:04:46 +0800

    Add a Width Upper Limit to draw_string

--- YookaiC/Game/SDLEx/RenderUtils.c
+++ YookaiC/Game/SDLEx/RenderUtils.c
@@ -9,10 +9,11 @@ void draw_texture(SDL_Renderer * renderer, SDL_Texture * texture, SDL_Point posi
 	SDL_RenderCopy(renderer, texture, NULL, &drawRect);
 }
 void draw_string(SDL_Renderer * renderer, TTF_Font * font, char * str, SDL_Point position) {
-	SDL_Color defaultColor = {.a = 255u, .b = 255u, .g = 255u, .r = 255u};
+	SDL_Color defaultColor = { .a = 255u,.b = 255u,.g = 255u,.r = 255u };
 	int textWidth = 0;
 	TTF_SizeUTF8(font, str, &textWidth, NULL);
-	SDL_Surface * textSurface = TTF_RenderUTF8_Blended_Wrapped(font, str, defaultColor, textWidth);
+	
+	SDL_Surface * textSurface = TTF_RenderUTF8_Blended_Wrapped(font, str, defaultColor, SDL_min(textWidth, 1024u));
 	SDL_Texture * tempTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
 	SDL_FreeSurface(textSurface);
 	draw_texture(renderer, tempTexture, position);
