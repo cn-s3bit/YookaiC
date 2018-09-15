dbdab83f60fd3e45856d2b2d8e08c47cdaa1d3e9
北海若 <shiruoxi2010@163.com>
2018-09-15 16:32:39 +0800

    SDL Flavored File API

--- YookaiC/Game/SDLEx/FileUtils.c
+++ YookaiC/Game/SDLEx/FileUtils.c
@@ -2,20 +2,21 @@
 #include "SDLWithPlugins.h"
 
 char * read_file_to_char_array(const char * filename, size_t * size_out) {
-	FILE * fp = fopen(filename, "r");
+	SDL_RWops * fp = SDL_RWFromFile(filename, "rb");
 	if (!fp) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Read File Error: fopen returns NULL\n");
 		return NULL;
 	}
-	long size = ftell(fp);
-	if (size < 0) {
+	Sint64 size = SDL_RWsize(fp);
+	if (size < 0 || size > SDL_MAX_SINT32) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Read File Error: ftell returns %d\n", size);
 		return NULL;
 	}
-	char * buffer = malloc(size);
-	fseek(fp, 0, 0);
-	fread(buffer, size, 1, fp);
-	fclose(fp);
-	*size_out = size;
+	size_t rsize = (size_t) size;
+	char * buffer = malloc(rsize);
+	SDL_RWseek(fp, 0, 0);
+	SDL_Log("Tried to read %d from %s, returns %d", rsize, filename, SDL_RWread(fp, buffer, rsize, 1));
+	SDL_RWclose(fp);
+	*size_out = rsize;
 	return buffer;
 }
