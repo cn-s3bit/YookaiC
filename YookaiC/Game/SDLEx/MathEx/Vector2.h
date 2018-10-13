38190863d57333dd9f12883f42848679638bed9d
北海若 <shiruoxi2010@163.com>
2018-10-13 11:12:23 +0800

    More Vector2 Operations

--- YookaiC/Game/SDLEx/MathEx/Vector2.h
+++ YookaiC/Game/SDLEx/MathEx/Vector2.h
@@ -33,6 +33,46 @@ inline Vector2 vector2_add(Vector2 a, Vector2 b) {
 	return a;
 }
 
+inline Vector2 vector2_adds(Vector2 a, float x, float y) {
+	a.X += x;
+	a.Y += y;
+	return a;
+}
+
+inline Vector2 vector2_sub(Vector2 a, Vector2 b) {
+	a.X -= b.X;
+	a.Y -= b.Y;
+	return a;
+}
+
+inline Vector2 vector2_subs(Vector2 a, float x, float y) {
+	a.X -= x;
+	a.Y -= y;
+	return a;
+}
+
+inline float vector2_lenSqr(Vector2 a) {
+	return a.X * a.X + a.Y + a.Y;
+}
+
+inline float vector2_len(Vector2 a) {
+	return SDL_sqrtf(vector2_lenSqr(a));
+}
+
+inline Vector2 vector2_scl(Vector2 a, float scalar) {
+	a.X *= scalar;
+	a.Y *= scalar;
+	return a;
+}
+
+inline Vector2 vector2_unit(Vector2 a) {
+	return vector2_scl(a, 1.0f / vector2_len(a));
+}
+
+inline float vector2_dot(Vector2 a, Vector2 b) {
+	return a.X * b.X + a.Y * b.Y;
+}
+
 #define vector2_sprintf(v, target) sprintf(target, "(%f, %f)", v.X, v.Y)
 #define vector2_sprintf_s(v, target, maxlen) sprintf_s(target, maxlen, "(%f, %f)", v.X, v.Y)
 #define vector2_sprintf_append(v, target) sprintf(target, "%s(%f, %f)", target, v.X, v.Y)
