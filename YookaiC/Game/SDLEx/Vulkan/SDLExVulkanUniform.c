843c453d01919e314e9490c8013bd82010b711d4
北海若 <shiruoxi2010@163.com>
2018-10-13 22:47:48 +0800

    Desceiptor Sets

---
+++ YookaiC/Game/SDLEx/Vulkan/SDLExVulkanUniform.c
@@ -0,0 +1,61 @@
+#include "SDLExVulkan.h"
+VkDescriptorPool VulkanDescriptorPool;
+extern VkDescriptorSetLayout VulkanDescriptorSetLayout;
+
+void create_descriptor_pool() {
+	VkDescriptorPoolSize poolSize = { .type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER };
+	poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+	poolSize.descriptorCount = get_vk_swap_chain()->ImageCount;
+
+	VkDescriptorPoolCreateInfo poolInfo = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO };
+	poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+	poolInfo.poolSizeCount = 1;
+	poolInfo.pPoolSizes = &poolSize;
+	poolInfo.maxSets = poolSize.descriptorCount;
+
+	if (vkCreateDescriptorPool(get_vk_device(), &poolInfo, NULL, &VulkanDescriptorPool) != VK_SUCCESS) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to Create Descriptor Pool!");
+	}
+	create_descriptor_sets();
+}
+
+void cleanup_descriptor_pool() {
+	vkDestroyDescriptorPool(get_vk_device(), VulkanDescriptorPool, NULL);
+}
+
+void bind_texture(VkImageView textureImageView, VkSampler textureSampler) {
+	unsigned n = get_vk_swap_chain()->ImageCount;
+	for (unsigned i = 0; i < n; i++) {
+		VkDescriptorImageInfo imageInfo;
+		imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+		imageInfo.imageView = textureImageView;
+		imageInfo.sampler = textureSampler;
+
+		VkWriteDescriptorSet descriptorWrites;
+		descriptorWrites.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrites.dstSet = get_vk_pipeline()->DescriptorSets[i];
+		descriptorWrites.dstBinding = 1;
+		descriptorWrites.dstArrayElement = 0;
+		descriptorWrites.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrites.descriptorCount = 1;
+		descriptorWrites.pImageInfo = &imageInfo;
+
+		vkUpdateDescriptorSets(get_vk_device(), 1, &descriptorWrites, 0, NULL);
+	}
+}
+
+void create_descriptor_sets() {
+	VkDescriptorSetLayout * layouts = malloc(sizeof(VkDescriptorSetLayout) * get_vk_swap_chain()->ImageCount);
+	for (unsigned i = 0; i < get_vk_swap_chain()->ImageCount; i++)
+		layouts[i] = VulkanDescriptorSetLayout;
+	VkDescriptorSetAllocateInfo allocInfo = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO };
+	allocInfo.descriptorPool = VulkanDescriptorPool;
+	allocInfo.descriptorSetCount = get_vk_swap_chain()->ImageCount;
+	allocInfo.pSetLayouts = layouts;
+
+	get_vk_pipeline()->DescriptorSets = malloc(sizeof(VkDescriptorSet) * allocInfo.descriptorSetCount);
+
+	if (vkAllocateDescriptorSets(get_vk_device(), &allocInfo, get_vk_pipeline()->DescriptorSets) != VK_SUCCESS) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to Allocate Descriptor Sets!");
+	}
+}
