9e62f7a3b048dd94c9a94ca740b4da81af0f986a
北海若 <shiruoxi2010@163.com>
2018-10-28 11:56:40 +0800

    Deque

---
+++ YookaiC/Game/SDLEx/Utils/Deque.c
@@ -0,0 +1,112 @@
+#include "Deque.h"
+#include <stdlib.h>
+#include <string.h>
+
+Deque * create_deque(unsigned elementSize, unsigned initialCapacity) {
+	Deque * deque_obj = malloc(sizeof(Deque));
+	deque_obj->_arrayList.Size = 0;
+	deque_obj->_arrayList._capacity = initialCapacity;
+	deque_obj->_arrayList._data = malloc(elementSize * initialCapacity);
+	deque_obj->_arrayList.ElementSize = elementSize;
+	deque_obj->_head = deque_obj->_tail = initialCapacity / 2;
+	void * tmp_trash = malloc(elementSize);
+	for (unsigned i = 0; i < initialCapacity; i++) {
+		append_array_list((ArrayList *)deque_obj, tmp_trash);
+	}
+	free(tmp_trash);
+	return deque_obj;
+}
+void destroy_deque(Deque * deque) {
+	free(deque->_arrayList._data);
+	free(deque);
+}
+int push_deque_tail(Deque * deque, const void * inItem) {
+	++deque->_tail;
+	if (deque->_tail >= (int)deque->_arrayList.Size) {
+		deque->_tail -= deque->_arrayList.Size;
+	}
+	if (deque->_tail == deque->_head) {
+		// expand
+		unsigned expand = deque->_arrayList.Size;
+		void * tmp_trash = malloc(deque->_arrayList.ElementSize);
+		for (unsigned i = 0; i < expand; i++) {
+			append_array_list((ArrayList *)deque, tmp_trash);
+		}
+		memcpy(
+			((char *)deque->_arrayList._data) + deque->_arrayList.ElementSize * expand,
+			((char *)deque->_arrayList._data),
+			deque->_arrayList.ElementSize * expand
+		);
+		deque->_tail += expand;
+		free(tmp_trash);
+	}
+	return assign_array_list_element((ArrayList *)deque, deque->_tail, inItem);
+}
+int poll_deque_head(Deque * deque, void * outItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	int ret = get_element_from_array_list((ArrayList *)deque, deque->_head, outItem);
+	if (ret != 0)
+		return ret;
+	++deque->_head;
+	if (deque->_head >= (int)deque->_arrayList.Size) {
+		deque->_head -= deque->_arrayList.Size;
+	}
+	return 0;
+}
+int peek_deque_head(Deque * deque, void * outItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	return get_element_from_array_list((ArrayList *)deque, deque->_head, outItem);
+}
+int push_deque_head(Deque * deque, const void * inItem) {
+	--deque->_head;
+	if (deque->_head < 0) {
+		deque->_head += deque->_arrayList.Size;
+	}
+	if (deque->_tail == deque->_head) {
+		// expand
+		unsigned expand = deque->_arrayList.Size;
+		void * tmp_trash = malloc(deque->_arrayList.ElementSize);
+		for (unsigned i = 0; i < expand; i++) {
+			append_array_list((ArrayList *)deque, tmp_trash);
+		}
+		memcpy(
+			((char *)deque->_arrayList._data) + deque->_arrayList.ElementSize * expand,
+			((char *)deque->_arrayList._data),
+			deque->_arrayList.ElementSize * expand
+		);
+		deque->_tail += expand;
+		free(tmp_trash);
+	}
+	return assign_array_list_element((ArrayList *)deque, deque->_head, inItem);
+}
+int poll_deque_tail(Deque * deque, void * outItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	int ret = get_element_from_array_list((ArrayList *)deque, deque->_tail, outItem);
+	if (ret != 0)
+		return ret;
+	--deque->_tail;
+	if (deque->_tail < 0) {
+		deque->_tail += deque->_arrayList.Size;
+	}
+	return 0;
+}
+int peek_deque_tail(Deque * deque, void * outItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	return get_element_from_array_list((ArrayList *)deque, deque->_tail, outItem);
+}
+int assign_deque_element(Deque * deque, unsigned index_from_head, const void * inItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	unsigned real_index = deque->_head + index_from_head % (deque->_tail - deque->_head);
+	return assign_array_list_element((ArrayList *)deque, real_index, inItem);
+}
+int get_deque_element(Deque * deque, unsigned index_from_head, void * outItem) {
+	if (deque->_head == deque->_tail)
+		return DEQUEUE_NO_ITEM;
+	unsigned real_index = deque->_head + index_from_head % (deque->_tail - deque->_head);
+	return get_element_from_array_list((ArrayList *)deque, real_index, outItem);
+}
---
+++ YookaiC/Game/SDLEx/Utils/Deque.h
@@ -0,0 +1,20 @@
+#ifndef SDLEX_DEQUE_H
+#define SDLEX_DEQUE_H
+#include "ArrayList.h"
+#define DEQUEUE_NO_ITEM 2
+typedef struct Deque {
+	ArrayList _arrayList;
+	int _head, _tail;
+} Deque;
+
+Deque * create_deque(unsigned elementSize, unsigned initialCapacity);
+void destroy_deque(Deque * deque);
+int push_deque_head(Deque * deque, const void * inItem);
+int poll_deque_head(Deque * deque, void * outItem);
+int peek_deque_head(Deque * deque, void * outItem);
+int push_deque_tail(Deque * deque, const void * inItem);
+int poll_deque_tail(Deque * deque, void * outItem);
+int peek_deque_tail(Deque * deque, void * outItem);
+int assign_deque_element(Deque * deque, unsigned index_from_head, const void * inItem);
+int get_deque_element(Deque * deque, unsigned index_from_head, void * outItem);
+#endif
