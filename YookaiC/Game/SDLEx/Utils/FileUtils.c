de8b01d4de30eb331101d33b04bf18851983e09b
北海若 <shiruoxi2010@163.com>
2018-12-07 21:36:24 +0800

    FileUtil: Add a EOF \0 at EOF

--- YookaiC/Game/SDLEx/Utils/FileUtils.c
+++ YookaiC/Game/SDLEx/Utils/FileUtils.c
@@ -13,10 +13,11 @@ char * read_file_to_char_array(const char * filename, size_t * size_out) {
 		return NULL;
 	}
 	size_t rsize = (size_t) size;
-	char * buffer = malloc(rsize);
+	char * buffer = malloc(rsize + 1);
 	SDL_RWseek(fp, 0, 0);
 	SDL_Log("Tried to read %d from %s, returns %d\n", rsize, filename, SDL_RWread(fp, buffer, rsize, 1));
 	SDL_RWclose(fp);
 	*size_out = rsize;
+	buffer[rsize] = '\0';
 	return buffer;
 }
