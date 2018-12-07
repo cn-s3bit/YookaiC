9019a6d386c765608276457bd677dded146f6203
??????·?¨?? <shiruoxi2010@163.com>
2018-12-07 22:50:26 +0800

    Add Constants

--- YookaiC/Game/Constants.h
+++ YookaiC/Game/Constants.h
@@ -1,6 +1,7 @@
 #ifndef YOOKAIC_CONSTANTS_H
 #define YOOKAIC_CONSTANTS_H
 #define RESOURCE_FOLDER "../Resources/"
+#define PYTHON_FOLDER "../Py367"
 #define DEFAULT_FONT_PATH RESOURCE_FOLDER "Fonts/LLT.ttc"
 
 #define EXIT_SIGNAL 32767
--- YookaiC/Game/EntryPoint.c
+++ YookaiC/Game/EntryPoint.c
@@ -37,6 +37,8 @@ int handle_event(void) {
 }
 
 int main(int argc, char ** argv) {
+	SDL_setenv("PYTHONHOME", PYTHON_FOLDER, 1);
+	SDL_setenv("PYTHONPATH", PYTHON_FOLDER "/DLLs;" PYTHON_FOLDER "/Lib", 1);
 	if (argc > 0)
 		SDL_Log("Working Path: %s\n", argv[0]);
 	wchar_t * program = Py_DecodeLocale(argv[0], NULL);
@@ -46,37 +48,7 @@ int main(int argc, char ** argv) {
 	}
 	Py_SetProgramName(program);  /* optional but recommended */
 	Py_Initialize();
-	size_t size;
-	const char * code =
-		"s = \"\"\"Gur Mra bs Clguba, ol Gvz Crgref\n"
-		"\n"
-		"Ornhgvshy vf orggre guna htyl.\n"
-		"Rkcyvpvg vf orggre guna vzcyvpvg.\n"
-		"Fvzcyr vf orggre guna pbzcyrk.\n"
-		"Pbzcyrk vf orggre guna pbzcyvpngrq.\n"
-		"Syng vf orggre guna arfgrq.\n"
-		"Fcnefr vf orggre guna qrafr.\n"
-		"Ernqnovyvgl pbhagf.\n"
-		"Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.\n"
-		"Nygubhtu cenpgvpnyvgl orngf chevgl.\n"
-		"Reebef fubhyq arire cnff fvyragyl.\n"
-		"Hayrff rkcyvpvgyl fvyraprq.\n"
-		"Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.\n"
-		"Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.\n"
-		"Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.\n"
-		"Abj vf orggre guna arire.\n"
-		"Nygubhtu arire vf bsgra orggre guna *evtug* abj.\n"
-		"Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.\n"
-		"Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.\n"
-		"Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!\"\"\"\n"
-		"\n"
-		"d = {}\n"
-		"for c in (65, 97):\n"
-		"	for i in range(26):\n"
-		"		d[chr(i + c)] = chr((i + 13) % 26 + c)\n"
-		"\n"
-		"print(\"\".join([d.get(c, c) for c in s]))\n";
-	PyRun_SimpleString(code);
+	PyRun_SimpleString("import this");
 	if (Py_FinalizeEx() < 0) {
 		exit(120);
 	}
