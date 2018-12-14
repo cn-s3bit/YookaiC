e63bb7584cef0c9eae4e8cf55a07bc43b4529f8a
北海若 <shiruoxi2010@163.com>
2018-12-14 22:22:59 +0800

    Very Basics

--- Py367/Lib/yoogic/abc/__init__.py
+++ Py367/Lib/yoogic/abc/__init__.py
@@ -1,3 +1,4 @@
 """
 Yoogic: Abstract Base Classes (Most of Engine Code)
 """
+from . import player, entity_basics
---
+++ Py367/Lib/yoogic/abc/entity_basics.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+class AbstractEntity(object):
+    pass
